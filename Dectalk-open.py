9e897fbd9db21d24e33b7963d4a1413b4f3d3347
Stuart Ward <stuart.ward@bcs.org>
2025-03-15 16:51:23 +0000

    Add an Opening Ceromony App

---
+++ Dectalk-open.py
@@ -0,0 +1,117 @@
+#! /usr/bin/python3
+
+# libraries Import
+from tkinter import ttk
+import tkinter
+import tkinter.scrolledtext
+from threading import Thread
+import serial
+from queue import Queue
+import time
+import random
+
+story2= [
+    "[:np]",
+    "Hello. I am a DECTalk text-to-speech synthesizer.",
+    "I was developed by Digital Equipment Corporation in the early 1980s.",
+    "This paticular unit was preserved by Edward Hammond.",
+    "I was restored to operation by The Redding Repair Cafe,",
+    "at the Redding Hackspace.",
+    "You may have herd my voice used by Stephen Hawking.",
+    "I am proud to be surrounded by so many other friends from the past.",
+    "But today I have the honor of opening the Readding Musiums exhibition.",
+    "I therefore declare the exhibition open.",
+    "Thank you."
+]
+
+# Main Window Properties
+window = tkinter.Tk()
+window.title("DECTalk Open Exabition")
+window.geometry("900x450")
+
+
+# Functions
+def speakdelay(prompt):
+    delay = max(int(len(prompt)*0.15), 2)
+    return delay
+
+def Button_id1_command():   #Story
+    for line in story2:
+        DECtalkQueue.put(line+"\n")
+    ProgressBar = Thread(target=SpeakProgressBar, args=[30])
+    ProgressBar.start()
+    pass
+
+def SpeakProgressBar(DelaySeconds):
+    Button_id1["state"] = "disabled"
+    ProgressBar_id0["value"] = 0
+    ProgressBar_id0["maximum"] = DelaySeconds
+    for i in range(DelaySeconds):
+        ProgressBar_id0["value"] = i
+        window.update()
+        time.sleep(1)
+    ProgressBar_id0["value"] = DelaySeconds
+    window.update()
+    time.sleep(1)
+    ProgressBar_id0["value"] = 0
+    Button_id1["state"] = "normal"
+    window.update()
+    pass
+
+def DECtalkSpeak(DECTalkQueue):
+    try:
+        DECTalk = serial.Serial(port="/dev/ttyUSB0", baudrate=9600, bytesize=8, parity="N", stopbits=2, xonxoff=True, rtscts=True)
+        Connected = True
+    except:
+        Connected = False
+    while True:
+        prompt = DECTalkQueue.get().encode("ascii")
+        print(prompt)
+        if Connected:
+            DECTalk.write(prompt)
+            DECTalk.flush()
+        time.sleep(int(len(prompt)*0.07))
+    pass
+
+# Widgets
+
+Label_id3 = tkinter.Label(
+    master=window,
+    text="Click this and the DECTalk will open the exabition:",
+    font=("Arial", 14),
+    height=1,
+    width=40,
+    bg="#FFFFFF",
+    fg="#222222",
+    justify="right"
+    )
+
+Button_id1 = tkinter.Button(
+    master=window,
+    text="Open Exabition",
+    font=("Arial", 20),
+    height=1,
+    width=20,
+    borderwidth=2,
+    bg="#FFFFFF",
+    fg="#222222",
+    command=Button_id1_command
+    )
+Button_id1.place(x=280, y=290)
+ProgressBar_id0 = ttk.Progressbar(
+    master=window,
+    orient="horizontal",
+    mode="determinate",
+    length=400,
+    )
+ProgressBar_id0.place(x=100, y=400)
+
+#start the DECTalk thread
+DECtalkQueue = Queue()
+DECtalkQueue.put("[:np]Starting the DECTalk system.\n")
+DECtalkThread = Thread(target=DECtalkSpeak, args=[DECtalkQueue], daemon=True)
+DECtalkThread.start()
+
+
+#run the main loop
+window.mainloop()
